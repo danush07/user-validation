8ebe21ec8e18d08b3173e9969599a24239b26141
Danush Hari <danushhari2001@gmail.com>
2023-12-12 20:44:31 +0530

    Update EditUserCanvas.jsx

--- components/EditUserCanvas.jsx
+++ components/EditUserCanvas.jsx
@@ -195,7 +195,7 @@ useEffect(() => {
     <div className="text-black">
       <div
         id="drawer-form"
-        className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform ease-in-out  w-2/3 dark:bg-gray-800"
+        className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform ease-in-out bg-white   w-2/3 dark:bg-gray-800"
         tabIndex="-1"
         aria-labelledby="drawer-form-label"
       >
